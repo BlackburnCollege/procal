9da9fbe2373fffff550060dddea23f0ca29ed47a
Drew Hans <drewhans555@gmail.com>
2017-10-19 18:55:14 -0500

    DrewHans - Made some changes... it should work now.

--- ProgrammerCalculator/app/src/main/java/edu/blackburn/programmercalculator/MainActivity.java
+++ ProgrammerCalculator/app/src/main/java/edu/blackburn/programmercalculator/MainActivity.java
@@ -16,4 +16,108 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }//end onCreate method
 
+    public void onPress0(View view) {
+    }
+
+    public void onPress2(View view) {
+    }
+
+    public void onPress1(View view) {
+    }
+
+    public void onPressEqual(View view) {
+    }
+
+    public void onPress4(View view) {
+    }
+
+    public void onPress7(View view) {
+    }
+
+    public void onPressA(View view) {
+    }
+
+    public void onPressD(View view) {
+    }
+
+    public void onPressAnd(View view) {
+    }
+
+    public void onPress5(View view) {
+    }
+
+    public void onPress8(View view) {
+    }
+
+    public void onPressB(View view) {
+    }
+
+    public void onPressE(View view) {
+    }
+
+    public void onPressNor(View view) {
+    }
+
+    public void onPressOr(View view) {
+    }
+
+    public void onPressNand(View view) {
+    }
+
+    public void onPress3(View view) {
+    }
+
+    public void onPress6(View view) {
+    }
+
+    public void onPress9(View view) {
+    }
+
+    public void onPressC(View view) {
+    }
+
+    public void onPressF(View view) {
+    }
+
+    public void onPressNxor(View view) {
+    }
+
+    public void onPressXor(View view) {
+    }
+
+    public void onPressAc(View view) {
+    }
+
+    public void onPressDel(View view) {
+    }
+
+    public void onPressNot(View view) {
+    }
+
+    public void onPressMod(View view) {
+    }
+
+    public void onPressDivide(View view) {
+    }
+
+    public void onPressMultiply(View view) {
+    }
+
+    public void onPressAdd(View view) {
+    }
+
+    public void onPressSubtract(View view) {
+    }
+
+    public void onPressDec(View view) {
+    }
+
+    public void onPressHex(View view) {
+    }
+
+    public void onPressOct(View view) {
+    }
+
+    public void onPressBin(View view) {
+    }
 }//end MainActivity class
