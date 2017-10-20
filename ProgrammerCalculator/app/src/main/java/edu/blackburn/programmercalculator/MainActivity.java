0620f86b83d18b3dfc40757053bba1c3bff1b9b0
Drew Hans <drewhans555@gmail.com>
2017-10-19 19:00:12 -0500

    DrewHans - Added toast...

--- ProgrammerCalculator/app/src/main/java/edu/blackburn/programmercalculator/MainActivity.java
+++ ProgrammerCalculator/app/src/main/java/edu/blackburn/programmercalculator/MainActivity.java
@@ -1,10 +1,12 @@
 package edu.blackburn.programmercalculator;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Model calculatorModel = new Model();
@@ -26,6 +28,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onPressEqual(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "Tim doesn't know how to use Android Studio!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
     public void onPress4(View view) {
