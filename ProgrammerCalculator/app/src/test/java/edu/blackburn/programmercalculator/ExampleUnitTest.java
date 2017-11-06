211c1c6d87d3bc7f22c30e5ca93acf4d08d1bb41
JessicaCramer117 <jessica.cramer@blackburn.edu>
2017-11-06 17:41:50 -0600

    Created methods for Unit Testing

--- ProgrammerCalculator/app/src/test/java/edu/blackburn/programmercalculator/ExampleUnitTest.java
+++ ProgrammerCalculator/app/src/test/java/edu/blackburn/programmercalculator/ExampleUnitTest.java
@@ -10,8 +10,137 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    Model calculator = new Model();
+    boolean signed = true;
+    boolean unsigned = false;
+    int bp8 = 8;
+    int bp16 = 16;
+    int bp32 = 32;
+
+
     @Test
-    public void addition_isCorrect() throws Exception {
+    public void add_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+
     }
+
+    @Test
+    public void sub_isCorrect() throws Exception {
+        assertEquals(4, 5 - 1);
+    }
+
+    @Test
+    public void div_isCorrect() throws Exception {
+
+    }
+
+    @Test
+    public void mul_isCorrect() throws Exception {
+
+    }
+
+    @Test
+    public void mod_isCorrect() throws Exception {
+
+    }
+
+    @Test
+    public void or_isCorrect() throws Exception {
+
+    }
+
+    @Test
+    public void xor_isCorrect() throws Exception {
+
+    }
+
+    @Test
+    public void not_isCorrect() throws Exception {
+
+    }
+
+    @Test
+    public void and_isCorrect() throws Exception {
+
+    }
+    @Test
+    public void nor_isCorrect() throws Exception {
+
+    }
+
+    @Test
+    public void xnor_isCorrect() throws Exception {
+
+    }
+
+    @Test
+    public void nand_isCorrect() throws Exception {
+
+    }
+
+    @Test
+    public void base2to8_isCorrect() throws Exception {
+
+    }
+
+    @Test
+    public void base2to10_isCorrect() throws Exception {
+
+    }
+
+    @Test
+    public void base2to16_isCorrect() throws Exception {
+
+    }
+
+    @Test
+    public void base8to2_isCorrect() throws Exception {
+
+    }
+
+    @Test
+    public void base8to10_isCorrect() throws Exception {
+
+    }
+
+    @Test
+    public void base8to16_isCorrect() throws Exception {
+
+    }
+
+    @Test
+    public void base10to2_isCorrect() throws Exception {
+
+    }
+
+    @Test
+    public void base10to8_isCorrect() throws Exception {
+
+    }
+
+    @Test
+    public void base10to16_isCorrect() throws Exception {
+
+    }
+
+    @Test
+    public void base16to2_isCorrect() throws Exception {
+
+    }
+
+    @Test
+    public void base16to8_isCorrect() throws Exception {
+
+    }
+
+    @Test
+    public void base16to10_isCorrect() throws Exception {
+
+    }
+
+    @Test
+    public void binaryAddition_isCorrect() throws Exception {
+
+    }
+
 }
